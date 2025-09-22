b301bbf9edf16a23fe6d49097bae5619911355af
新垣来斗 <moyashino_gohan@MacBook-Air-2.local>
2025-09-22 19:21:45 +0900

    Fix React Router for deployment

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import BlogPage from './compornents/BlogPage';
 import Header from './compornents/Header';
 import HomePage from './compornents/HomePage';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-      <Header />
+    <BrowserRouter basename="/Portfolio">
+        <div className="App">
+        <Header />
 
-        <Routes>
-          <Route path="/" element={< HomePage /> } />
-          <Route path="blog" element={< BlogPage />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={< HomePage /> } />
+            <Route path="blog" element={< BlogPage />} />
+          </Routes>
+        </div>
+    </BrowserRouter>
   );
 }
 
